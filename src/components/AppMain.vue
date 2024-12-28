654eb2da7eaa7ca86870cd332e1b665ec4cec31d
jacopolegovini <legovini96@gmail.com>
2024-12-28 18:47:48 +0100

    header created

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,7 +9,34 @@ export default {
 </script>
 
 <template>
-    test
+    <div class="page">
+        <header>
+            <img src="../../images/header-bg.png" alt="header">
+        </header>
+        <main>
+            <div class="icons">
+                <div class="icon" v-for="icon in 24">{{ icon }}</div>
+            </div>
+        </main>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+header img {
+    background-color: #2D28EB;
+}
+
+.icons {
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    flex-wrap: wrap;
+}
+
+.icon {
+    background-color: #2E6754;
+    border-radius: 10px;
+    width: 80px;
+    height: 80px;
+}
+</style>
